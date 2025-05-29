a1246113f27cd1ecc52af6bf76c397c991c43176
Gospel-Columbus <132568918+Gospel-Columbus@users.noreply.github.com>
2025-05-29 17:19:35 +0100

    commit these changes now

---
+++ external.js
@@ -0,0 +1,10 @@
+
+
+    function changing_text_one() {
+        document.getElementById("demo").innerHTML = "This is the First Paragraph."
+    }
+
+
+    function changing_text_two() {
+        document.getElementById("demo").innerHTML = "This is the Second Paragraph."
+    }
